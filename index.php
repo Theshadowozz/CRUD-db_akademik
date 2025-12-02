89ec6ab3f1374ae2b979bdc311e1a34b99aadfc9
Theshadowozz <rifqishadow098@gmail.com>
2025-12-02 13:14:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_akademik";
+    
+    $koneksi = mysqli_connect($host, $username, $password, $database);
+    
+    if(!$koneksi){
+        echo "database tidak terkoneksi : ". mysqli_connect_error();
+    }
+
+?>
